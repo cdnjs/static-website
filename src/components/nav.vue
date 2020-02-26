5291f7a7f6cff189302e42def81164558baccc6d
MattIPv4 <me@mattcowley.co.uk>
2020-02-26 12:08:54 +0000

    Start building a nav component

---
+++ src/components/nav.vue
@@ -0,0 +1,9 @@
+<template>
+    <nav class="nav">
+        <div class="left">
+            <a href="/"><img src="../assets/logo.svg" alt="< cdnjs >" /></a>
+            <a href="https://twitter.com/cdnjs"><i class="fab fa-twitter"></i></a>
+            <a href="https://cdnjs.discourse.group/"><i class="fab fa-discourse"></i></a>
+        </div>
+    </nav>
+</template>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,20 +1,20 @@
 <template>
-    <div>
-        <div class="nav">
-            <a href="https://twitter.com/cdnjs"><i class="fab fa-twitter"></i></a>
-            <a href="https://cdnjs.discourse.group/"><i class="fab fa-discourse"></i></a>
-            <a href="https://github.com/MattIPv4/cdnjs.dev"><i class="fab fa-github"></i></a>
-        </div>
-
-        <div class="main">
+    <main>
+        <Nav></Nav>
+        <section class="main">
             <a href="https://cdnjs.com"><img src="../assets/logo.svg" alt="cdnjs"/></a>
             <h1>Coming soon</h1>
-        </div>
-    </div>
+        </section>
+    </main>
 </template>
 
 <script>
+    const Nav = require('../components/nav');
+
     module.exports = {
         name: 'Index',
+        components: {
+            Nav,
+        }
     };
 </script>
