fa7bfd13e860e6547a39d2cea5341801a53475d1
MattIPv4 <me@mattcowley.co.uk>
2020-03-15 17:14:49 +0000

    Improve responsiveness loads

--- components/footer.vue
+++ components/footer.vue
@@ -1,111 +1,113 @@
 <template>
     <footer>
-        <dl>
-            <dd class="logo">
-                <nuxt-link :to="{ name: 'index' }">
-                    <img src="../assets/img/logo.svg" alt="< cdnjs >" />
-                </nuxt-link>
-            </dd>
-            <dd class="copy-social">
-                <span>
-                    &copy; {{ (new Date()).getFullYear().toString() }} cdnjs.
-                </span>
-                <a href="https://twitter.com/cdnjs?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer"
-                   aria-label="cdnjs on Twitter"
-                >
-                    <i class="fab fa-twitter" aria-hidden="true"></i>
-                </a>
-                <a href="https://github.com/cdnjs?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer"
-                   aria-label="cdnjs on GitHub"
-                >
-                    <i class="fab fa-github" aria-hidden="true"></i>
-                </a>
-            </dd>
-        </dl>
+        <div class="content">
+            <dl>
+                <dd class="logo">
+                    <nuxt-link :to="{ name: 'index' }">
+                        <img src="../assets/img/logo.svg" alt="< cdnjs >" />
+                    </nuxt-link>
+                </dd>
+                <dd class="copy-social">
+                    <span>
+                        &copy; {{ (new Date()).getFullYear().toString() }} cdnjs.
+                    </span>
+                    <a href="https://twitter.com/cdnjs?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer"
+                       aria-label="cdnjs on Twitter"
+                    >
+                        <i class="fab fa-twitter" aria-hidden="true"></i>
+                    </a>
+                    <a href="https://github.com/cdnjs?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer"
+                       aria-label="cdnjs on GitHub"
+                    >
+                        <i class="fab fa-github" aria-hidden="true"></i>
+                    </a>
+                </dd>
+            </dl>
 
-        <dl>
-            <dt class="title">
-                About
-            </dt>
-            <dd>
-                <nuxt-link :to="{ name: 'about' }">
-                    About Us
-                </nuxt-link>
-            </dd>
-            <dd>
-                <nuxt-link :to="{ name: 'sponsor' }">
-                    Sponsor Us
-                </nuxt-link>
-            </dd>
-            <dd>
-                <a href="https://swag.cdnjs.com?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer">
-                    Swag Store
-                </a>
-            </dd>
+            <dl>
+                <dt class="title">
+                    About
+                </dt>
+                <dd>
+                    <nuxt-link :to="{ name: 'about' }">
+                        About Us
+                    </nuxt-link>
+                </dd>
+                <dd>
+                    <nuxt-link :to="{ name: 'sponsor' }">
+                        Sponsor Us
+                    </nuxt-link>
+                </dd>
+                <dd>
+                    <a href="https://swag.cdnjs.com?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer">
+                        Swag Store
+                    </a>
+                </dd>
 
-            <dd class="break"></dd>
+                <dd class="break"></dd>
 
-            <dd>
-                <a href="https://cdnjs.discourse.group?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer">
-                    Community Discourse
-                </a>
-            </dd>
-            <dd>
-                <a href="https://opencollective.com/cdnjs?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer">
-                    OpenCollective
-                </a>
-            </dd>
-            <dd>
-                <a href="https://www.patreon.com/cdnjs?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer">
-                    Patreon
-                </a>
-            </dd>
-            <dd>
-                <a href="https://www.cloudflare.com/network/?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer">
-                    CDN Network Map
-                </a>
-            </dd>
-        </dl>
+                <dd>
+                    <a href="https://cdnjs.discourse.group?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer">
+                        Community Discourse
+                    </a>
+                </dd>
+                <dd>
+                    <a href="https://opencollective.com/cdnjs?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer">
+                        OpenCollective
+                    </a>
+                </dd>
+                <dd>
+                    <a href="https://www.patreon.com/cdnjs?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer">
+                        Patreon
+                    </a>
+                </dd>
+                <dd>
+                    <a href="https://www.cloudflare.com/network/?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer">
+                        CDN Network Map
+                    </a>
+                </dd>
+            </dl>
 
-        <dl>
-            <dt>Libraries</dt>
-            <dd>
-                <nuxt-link :to="{ name: 'libraries' }">
-                    Search Libraries
-                </nuxt-link>
-            </dd>
-            <dd>
-                <nuxt-link :to="{ name: 'api' }">
-                    API Documentation
-                </nuxt-link>
-            </dd>
+            <dl>
+                <dt>Libraries</dt>
+                <dd>
+                    <nuxt-link :to="{ name: 'libraries' }">
+                        Search Libraries
+                    </nuxt-link>
+                </dd>
+                <dd>
+                    <nuxt-link :to="{ name: 'api' }">
+                        API Documentation
+                    </nuxt-link>
+                </dd>
 
-            <dt>Status</dt>
-            <dd>
-                <a href="https://status.cdnjs.com?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer">
-                    Status Page
-                </a>
-            </dd>
-            <dd>
-                <a href="https://twitter.com/cdnjsStatus?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer">
-                    cdnjsStatus on Twitter
-                </a>
-            </dd>
-            <dd>
-                <a href="http://stats.pingdom.com/4jg86a2wqei0?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer">
-                    CDN Uptime
-                </a>
-            </dd>
-        </dl>
+                <dt>Status</dt>
+                <dd>
+                    <a href="https://status.cdnjs.com?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer">
+                        Status Page
+                    </a>
+                </dd>
+                <dd>
+                    <a href="https://twitter.com/cdnjsStatus?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer">
+                        cdnjsStatus on Twitter
+                    </a>
+                </dd>
+                <dd>
+                    <a href="http://stats.pingdom.com/4jg86a2wqei0?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer">
+                        CDN Uptime
+                    </a>
+                </dd>
+            </dl>
 
-        <dl>
-            <dt>Sponsors</dt>
-            <dd v-for="sponsor in sponsors">
-                <a :href="`${sponsor.site}?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer`">
-                    {{ sponsor.name }}
-                </a>
-            </dd>
-        </dl>
+            <dl>
+                <dt>Sponsors</dt>
+                <dd v-for="sponsor in sponsors">
+                    <a :href="`${sponsor.site}?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_footer`">
+                        {{ sponsor.name }}
+                    </a>
+                </dd>
+            </dl>
+        </div>
     </footer>
 </template>
 
--- components/nav.vue
+++ components/nav.vue
@@ -1,41 +1,43 @@
 <template>
     <nav class="nav">
-        <div class="left">
-            <nuxt-link :to="{ name: 'index' }" class="logo">
-                <img src="../assets/img/logo.svg" alt="< cdnjs >" />
-            </nuxt-link>
-            <a href="https://twitter.com/cdnjs?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_nav"
-               aria-label="cdnjs on Twitter"
-            >
-                <i class="fab fa-twitter" aria-hidden="true"></i>
-            </a>
-            <a href="https://cdnjs.discourse.group/?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_nav"
-               aria-label="Community Discourse"
-            >
-                <i class="fab fa-discourse" aria-hidden="true"></i>
-            </a>
-        </div>
+        <div class="content">
+            <div class="left">
+                <nuxt-link :to="{ name: 'index' }" class="logo">
+                    <img src="../assets/img/logo.svg" alt="< cdnjs >" />
+                </nuxt-link>
+                <a href="https://twitter.com/cdnjs?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_nav"
+                   aria-label="cdnjs on Twitter"
+                >
+                    <i class="fab fa-twitter" aria-hidden="true"></i>
+                </a>
+                <a href="https://cdnjs.discourse.group/?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_nav"
+                   aria-label="Community Discourse"
+                >
+                    <i class="fab fa-discourse" aria-hidden="true"></i>
+                </a>
+            </div>
 
-        <div class="middle">
-            <slot></slot>
-        </div>
+            <div class="middle">
+                <slot></slot>
+            </div>
 
-        <div class="right">
-            <nuxt-link :to="{ name: 'about' }">
-                About
-            </nuxt-link>
-            <nuxt-link :to="{ name: 'libraries' }">
-                Libraries
-            </nuxt-link>
-            <nuxt-link :to="{ name: 'api' }">
-                API
-            </nuxt-link>
-            <a href="https://github.com/cdnjs?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_nav">
-                GitHub
-            </a>
-            <a href="https://status.cdnjs.com?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_nav">
-                Status <StatusIndicator page-id="1fkb7yl9sw87"></StatusIndicator>
-            </a>
+            <div class="right">
+                <nuxt-link :to="{ name: 'about' }">
+                    About
+                </nuxt-link>
+                <nuxt-link :to="{ name: 'libraries' }">
+                    Libraries
+                </nuxt-link>
+                <nuxt-link :to="{ name: 'api' }">
+                    API
+                </nuxt-link>
+                <a href="https://github.com/cdnjs?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_nav">
+                    GitHub
+                </a>
+                <a href="https://status.cdnjs.com?utm_source=cdnjs&utm_medium=link&utm_campaign=cdnjs_nav">
+                    Status <StatusIndicator page-id="1fkb7yl9sw87"></StatusIndicator>
+                </a>
+            </div>
         </div>
     </nav>
 </template>
