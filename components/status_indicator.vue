156effdc266db62f950e0c12cc88eb1f30f0b3a8
MattIPv4 <me@mattcowley.co.uk>
2020-04-25 11:26:43 +0100

    Add tooltip to status indicator

--- components/status_indicator.vue
+++ components/status_indicator.vue
@@ -1,14 +1,23 @@
 <template>
-    <span ref="dot"></span>
+    <span ref="dot"
+          @mouseenter="tooltipShow"
+          @mouseleave="tooltipHide"
+    ></span>
 </template>
 
 <script>
+    import tlite from 'tlite';
     import status from '../util/statuspage_status';
 
     export default {
         name: 'StatusIndicator',
         props: {
             pageId: String,
+        },
+        data () {
+            return {
+                status: null,
+            };
         },
         async mounted () {
             // Fetch initial status
@@ -22,7 +31,15 @@
         methods: {
             async updateStatus () {
                 const data = await status(this.$props.pageId);
+                this.$data.status = data.status;
                 this.$refs.dot.className = `status-indicator status-indicator-${data.status.indicator}`;
+            },
+            tooltipShow (evt) {
+                evt.target.setAttribute('data-tlite', this.$data.status.description);
+                tlite.show(evt.target);
+            },
+            tooltipHide (evt) {
+                tlite.hide(evt.target);
             },
         },
     };
