f547cabe422ae8251d51d20497129f186966ba7e
MattIPv4 <me@mattcowley.co.uk>
2020-06-23 12:07:07 +0100

    Provide a default downloadUrl in library JSON-LD if possible

--- data/json-ld/library.js
+++ data/json-ld/library.js
@@ -1,9 +1,17 @@
 import { getAssets } from '../../util/get_asset';
 
 export default (base, library, libraryName) => {
-    let downloadUrl; let softwareVersion = library.version;
+    let downloadUrl = library.filename ? `https://cdnjs.cloudflare.com/ajax/libs/${libraryName}/${library.version}/${library.filename}` : undefined;
+    let softwareVersion = library.version;
+
+    // This shouldn't ever be used as we don't fetch library.assets,
+    //  but it can be left in to provide better data if library.assets is ever present
     if (library.assets && library.assets.length) {
-        if (!library.assets.find(v => v.version === softwareVersion)) { softwareVersion = library.assets[0].version; }
+        if (!library.assets.find(v => v.version === softwareVersion)) {
+            softwareVersion = library.assets[0].version;
+        }
+
+        // This would return a Promise if version wasn't present in library.assets
         const { assets } = getAssets({ library, version: softwareVersion });
         downloadUrl = assets.map(asset => asset.url);
     }
