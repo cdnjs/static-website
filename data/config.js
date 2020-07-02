467b737cbe51ca0017e60018a7d971367e1f13b7
bmvermeer <brianvermeer@snyk.io>
2020-07-02 17:45:05 +0200

    moving config to config.js

--- data/config.js
+++ data/config.js
@@ -1,1 +1,3 @@
 export const baseApi = 'https://api.cdnjs.com';
+export const snykApi = 'http://snyk-widget.herokuapp.com';
+export const snykKey = 'VRHr?YP_Gb7d9LFh$ugktZHUe+gGBp#LGG3*bDLt';
--- util/get_vulns.js
+++ util/get_vulns.js
@@ -1,9 +1,7 @@
 import fetch from 'node-fetch';
-const snykApi = 'http://snyk-widget.herokuapp.com';
-const key = 'VRHr?YP_Gb7d9LFh$ugktZHUe+gGBp#LGG3*bDLt';
+import { snykApi, snykKey } from '../data/config';
 
 export default async (library, version) => {
-    // await new Promise(r => setTimeout(r, 10000));
     let libname = library.name;
     if (library.autoupdate && library.autoupdate.source === 'npm') {
         libname = library.autoupdate.target;
@@ -13,6 +11,6 @@ export default async (library, version) => {
     }
 
     const res = await fetch(`${snykApi}/test/npm/lib/${libname}/${version}`,
-        { headers: { Authorization: key } });
+        { headers: { Authorization: snykKey } });
     return res.json();
 };
