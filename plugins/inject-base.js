52c1bdbfe85b308604525c8ce75c7931f71c87dc
Alexander Lichter <manniL@gmx.net>
2020-09-11 14:09:20 +0200

    Update plugins/inject-base.js
    Co-authored-by: Matt (IPv4) Cowley <me@mattcowley.co.uk>

--- plugins/inject-base.js
+++ plugins/inject-base.js
@@ -1,6 +1,6 @@
 const origin = process.env.SITE_HOST ||
     (typeof (window) !== 'undefined' ? window.location.origin : '/');
-const base = origin + (origin.endsWith('/') ? '' : '/');
+const base = origin.trim().replace(/\/*$/, '') + '/';
 
 export default function (ctx, inject) {
     inject('base', base);
