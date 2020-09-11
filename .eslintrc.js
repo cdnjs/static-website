e7b01a6f496ace59d3875a5b0e43e61765ac4cfd
Alexander Lichter <github@lichter.io>
2020-09-11 12:59:45 +0200

    refactor: improve eslint command

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
     root: true,
     env: {
-        'browser': true,
-        'node': true
+        browser: true,
+        node: true,
     },
     extends: [
         'eslint:recommended',
