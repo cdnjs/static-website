3952fbce244335ef8a3b5d3566a52eac9fade1a2
Matt Cowley <mcowley@digitalocean.com>
2024-04-02 03:12:19 +0100

    Misc optimizations to reduce build time (#145)
    * Remove unused Procfile
    
    * Remove unused dependencies
    
    * Track timings for fetching library versions
    
    * Update to Node.js 16 / NPM 8
    
    * Swap to continual concurrency rather than chunks
    
    * Reduce concurrency to improve speed
    
    * Log the slowest libraries to fetch versions for
    
    * Update sass-loader to fix warning

--- util/build/routes.js
+++ util/build/routes.js
@@ -1,18 +1,27 @@
-import chunk from 'chunk';
 import fetch from 'node-fetch';
+import consola from 'consola';
 import { baseApi } from '../../data/config';
 
 export default async () => {
-    // Create the lib promises
+    // Fetch the library names
+    const libsStart = Date.now();
     const libsRaw = await fetch(`${baseApi}/libraries?fields=name`);
     const libsJson = await libsRaw.json();
+    consola.info(`  Fetched ${libsJson.results.length} libraries in ${Date.now() - libsStart}ms`);
+
+    // Track timings for individual libraries
+    const libsAsyncStart = Date.now();
+    const timings = [];
+
+    // Create the lib promises
     const libsAsync = libsJson.results.map((lib) => {
         return [lib.name, async () => {
+            const start = Date.now();
             const libRaw = await fetch(`${baseApi}/libraries/${encodeURIComponent(lib.name)}?fields=versions`);
             const libJson = await libRaw.json();
 
             if (!libJson.versions) {
-                console.warn(`No versions array for ${lib.name}`, libJson);
+                consola.warn(`No versions array for ${lib.name}`, libJson);
             }
 
             const versions = (libJson.versions || []).map((version) => {
@@ -21,6 +30,8 @@ export default async () => {
                     priority: 0.5,
                 };
             });
+
+            timings.push([lib.name, Date.now() - start]);
 
             return [
                 {
@@ -32,18 +43,32 @@ export default async () => {
         }];
     });
 
-    // Split into chunks and fetch
-    const libsChunks = chunk(libsAsync, 100);
-    const failed = [];
+    // Run with a continual concurrency of 10
     const libs = [];
-    for (const libsChunk of libsChunks) {
-        const chunkRes = await Promise.all(libsChunk.map(cb => cb[1]().catch(() => failed.push(cb))));
-        libs.push(...chunkRes.flat(1));
-    }
-    for (const failure of failed) {
-        const result = await failure[1]().catch(e => console.warn(failure[0], e));
+    const failed = [];
+    const libPromise = (name, callback) => callback()
+        .then(result => libs.push(...result))
+        .catch(() => failed.push([name, callback]))
+        .finally(() => {
+            const next = libsAsync.shift();
+            if (next) { return libPromise(...next); }
+        });
+    await Promise.all(libsAsync.splice(0, 10).map(next => libPromise(...next)));
+
+    // Re-run any failures with a concurrency of 1
+    for (const [name, callback] of failed) {
+        const result = await callback().catch(e => consola.warn(name, e));
         if (result) { libs.push(...result); }
     }
+
+    // Report the timings
+    timings.sort((a, b) => a[1] - b[1]);
+    consola.info(`  Fetched ${timings.length} libraries in ${Date.now() - libsAsyncStart}ms:`);
+    consola.info(`    p99: ${timings[Math.floor(timings.length * 0.99)][1]}ms`);
+    consola.info(`    p90: ${timings[Math.floor(timings.length * 0.9)][1]}ms`);
+    consola.info(`    p50: ${timings[Math.floor(timings.length * 0.5)][1]}ms`);
+    consola.info('  Slowest libraries:');
+    timings.slice(-5).reverse().forEach(([name, time]) => consola.info(`    ${name}: ${time}ms`));
 
     // Ensure everything is valid & sort by URL
     return libs.filter(x => !!x && !!x.url).sort((a, b) => a.url.localeCompare(b.url));
