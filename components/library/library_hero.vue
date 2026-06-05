7c233218f743094a65b48c5c9572439fd56637e1
“Jun” <junio.akarda26@gmail.com>
2026-06-05 19:32:44 +0700

    Remove the Snyk vulnerabilities widget
    The Snyk widget service (snyk-widget.herokuapp.com) has been returning errors
    for all libraries for around a year (see #170), with no plan to restore the data
    source. Remove it entirely: the get_vulns util, the shield in library_hero, and
    the snyk config, so its dead requests can no longer hang or clutter library pages.

--- components/library/library_hero.vue
+++ components/library/library_hero.vue
@@ -24,12 +24,6 @@
                     package
                 </a>
             </p>
-            <p v-if="vulns">
-                <a :href="vulns.url">
-                    <ShieldAlt class="icon" aria-label="Snyk"></ShieldAlt>
-                    {{ vulns.vulns }} known vulnerabilities
-                </a>
-            </p>
         </div>
 
         <div class="row">
@@ -62,10 +56,8 @@
     import Star from '@fortawesome/fontawesome-free/svgs/solid/star.svg?inline';
     import GitHub from '@fortawesome/fontawesome-free/svgs/brands/github.svg?inline';
     import Npm from '@fortawesome/fontawesome-free/svgs/brands/npm.svg?inline';
-    import ShieldAlt from '@fortawesome/fontawesome-free/svgs/solid/shield-alt.svg?inline';
     import formatUnits from '../../util/format_units';
     import utm from '../../util/utm';
-    import getVulns from '../../util/get_vulns';
 
     export default {
         name: 'LibraryHero',
@@ -73,27 +65,9 @@
             Star,
             GitHub,
             Npm,
-            ShieldAlt,
         },
         props: {
             library: Object,
-            version: String,
-        },
-        data () {
-            return {
-                vulns: null,
-            };
-        },
-        watch: {
-            version: {
-                immediate: true,
-                async handler () {
-                    this.$data.vulns = await getVulns(this.$props.library, this.$props.version).catch(() => {});
-                },
-            },
-        },
-        async created () {
-            this.$data.vulns = await getVulns(this.$props.library, this.$props.version).catch(() => {});
         },
         methods: {
             formatUnits,
--- data/config.js
+++ data/config.js
@@ -1,3 +1,1 @@
 export const baseApi = process.env.API_BASE || 'https://api.cdnjs.com';
-export const snykApi = 'https://snyk-widget.herokuapp.com';
-export const snykKey = 'VRHr?YP_Gb7d9LFh$ugktZHUe+gGBp#LGG3*bDLt';
